7ae56a7e734d5e3cd69b75fb38057dbc0e40adf8
chayanonz <52643193+chayanonz@users.noreply.github.com>
2020-02-19 21:51:23 +0700

    Start Use Git

--- model/BaseModel.js
+++ model/BaseModel.js
@@ -2,25 +2,25 @@ var mysql = require('mysql');
 
 // //local mysql db connection // // //
 
-var connection = mysql.createConnection({
-    connectionimit: 100,
-    host: 'localhost',
-    user: 'root',
-    port: 3306,
-    password: 'root123456',
-    database: 'database_slf',
-    multipleStatements: true
-});
-
 // var connection = mysql.createConnection({
 //     connectionimit: 100,
-//     host: 'it2.sut.ac.th',
-//     user: 'project62_g24',
+//     host: 'localhost',
+//     user: 'root',
 //     port: 3306,
-//     password: '404404',
-//     database: 'project62_g24',
+//     password: 'root123456',
+//     database: 'database_slf',
 //     multipleStatements: true
 // });
+
+var connection = mysql.createConnection({
+    connectionimit: 100,
+    host: 'it2.sut.ac.th',
+    user: 'project62_g24',
+    port: 3306,
+    password: '404404',
+    database: 'project62_g24',
+    multipleStatements: true
+});
 
 
 /*  Note 
